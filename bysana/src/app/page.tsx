7f0a927730bd703164645c8287f6593a782fdefe
sana-sagegami <sana01237@icloud.com>
2026-01-15 17:57:11 +0900

    feat: create Home component and update Page structure to use it

--- bysana/src/app/page.tsx
+++ bysana/src/app/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "./home/HomePage";
+import Home from "./home/Page";
 
-export default function Home() {
-  return <HomePage />;
+export default function Page() {
+  return <Home />;
 }
